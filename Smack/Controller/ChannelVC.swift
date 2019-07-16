c347f6676543cf83174a7b89354aeaa65f8dad47
daramfon <Daramfon@buffalo.edu>
2019-07-15 20:16:11 -0400

    SWReveal

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Daramfon Akpan on 7/14/19.
+//  Copyright © 2019 Daramfon Akpan. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.width - 60
+      
+    }
+    
+
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Daramfon Akpan on 7/13/19.
+//  Copyright © 2019 Daramfon Akpan. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer((self.revealViewController()?.panGestureRecognizer())!)
+        self.view.addGestureRecognizer(((self.revealViewController()?.tapGestureRecognizer())!))
+    }
+
+
+}
+
